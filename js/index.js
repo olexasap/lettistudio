0f6ac4740711cade96674d61e764fa291ae81a05
Olexa Sapronov <olexasap@gmail.com>
2022-06-13 20:50:06 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var menu = document.querySelector(".menu")
+var ham = document.querySelector(".ham")
+var xIcon = document.querySelector(".xIcon")
+var menuIcon = document.querySelector(".menuIcon")
+
+ham.addEventListener("click", toggleMenu)
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    xIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    xIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+var menuLinks = document.querySelectorAll(".menuLink")
+
+menuLinks.forEach(
+  function (menuLink) {
+    menuLink.addEventListener("click", toggleMenu)
+  }
+)
+
+
+document.querySelector('.examples__more').addEventListener('click', () => {
+  [...document.querySelectorAll('.examples__wrapp_active')].forEach(e=> e.classList.toggle('active-alb'))
+  document.querySelector('.examples__more').classList.toggle('examples__wrapp_active');
+});
+
+$(window).scroll(function(){
+  $('svg rect').toggleClass('new-rect', $(this).scrollTop() > 500);
+});
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
